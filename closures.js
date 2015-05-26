67a9ec837f4e52264ddf378fa9fd8902f083292f
Brandt <brandtbird@yahoo.com>
2015-05-25 22:58:48 -0600

    Answers to questions.

--- closures.js
+++ closures.js
@@ -10,11 +10,11 @@ var outer = function(){
 //Invoke outer saving the return value into another variable called 'inner'.
 
   //Code Here
-
+var inner = outer();
 //Once you do that, invoke inner.
 
   //Code Here
-
+inner()
 
 
 //Next problem
@@ -33,9 +33,15 @@ var callFriend = function(){
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
   //Code Here
+var callFriend = function(){
+  var friend = 'Jake';
+  function callF(number){
+    return 'Calling ' + friend + ' at ' + number;
+  }
+  return callF('435-215-9248');
+};
 
-
-
+alert(callFriend());
 //Next Problem
 
 
@@ -45,11 +51,17 @@ var callFriend = function(){
 */
 
   //Code Here
-  var count = makeCounter();
-  count() // 1
-  count() // 2
-  count() // 3
-  count() // 4
+var makeCounter = function() {
+  var increment = 0;
+  return function () {
+    return increment += 1;
+  };
+};
+var count = makeCounter();
+alert(count()); // 1
+alert(count()); // 2
+alert(count()); // 3
+alert(count()); // 4
 
 
 
@@ -63,7 +75,9 @@ var callFriend = function(){
 */
 
   //Code Here
+var firstFn = function(){
 
+};
 
 
 //Next Problem
@@ -89,17 +103,19 @@ var callFriend = function(){
     }
   };
 
+  counter()
+
   Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
 
     //Answer Here
-
+When counter is invoked--via counter()--the value of i will be console logged 5 times, each one taking a little more time before it logs.  You think it would start at 1 and increment to 5.  But, it will log that i = 5...five times.
 
   Now, run the function in your console and note what happpens.
 
   Was your answer right or wrong?
 
     //Answer Here
-
+In theory yes.  It showed that i = 6, five times.  I changed the code to alert rather than console.log.
 
   Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc)
 */
--- fns-and-scope.js
+++ fns-and-scope.js
@@ -1,11 +1,21 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
 
 
-var name = 'Tyler';
+var name = 'Brandt';
 //Create a function called isTyler that accepts name as it's only argument.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
+var isTyler = function(name) {
+  if (name === 'Tyler') {
+    return true;
+  }
+  else {
+    return false;
+  };
+}
+
+alert(isTyler(name));
 
 
 //Next problem
@@ -16,8 +26,11 @@ var name = 'Tyler';
 
 
   //Code Here
+var getName = function() {
+  return prompt("What is your name?");
+}
 
-
+alert(getName());
 //Next Problem
 
 
@@ -26,8 +39,11 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
+var welcome = function() {
+  return alert("Welcome, " + getName() + "!");
+}
 
-
+welcome()
 //Next problem
 
 
@@ -36,7 +52,9 @@ var name = 'Tyler';
 //What is the difference between arguments and parameters?
 
   //Answer Here
+The variable(s) inside of the parentheses in function statement is the parameter(s).
 
+The data being passed to the function in the function call, inside the parentheses, is the argument(s).
 
 //Next problem
 
@@ -46,8 +64,12 @@ var name = 'Tyler';
 
 
   //Answer Here
-
-
+false
+0 (zero)
+"" (empty string)
+null
+undefined
+NaN
 
 //Next Problem
 
@@ -56,14 +78,21 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
+var myName = function() {
+  return alert("Brandt");
+}
+
+myName()
 
 //Now save the function definition of myName into a new variable called newMyName
 
   //Code Here
-
+var newMyName = function() {
+  myName();
+}
 //Now alert the result of invoking newMyName
 
-
+newMyName()
 
 //Next problem
 
@@ -72,9 +101,18 @@ var name = 'Tyler';
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
+var outerFn = function() {
+  return function() {
+    return alert("Brandt");
+  }
+}
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
+var innerFn = outerFn();
+
+
 
 //Now invoke innerFn.
+innerFn()
